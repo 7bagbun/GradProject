dfec3fe8194ee2333e5b9a6cc80a1d156c2acad6
7bagbun <s9010436@gmail.com>
2023-12-09 20:29:15 +0800

    1.新增了撰寫公告時的alert提示 且會透過cookie判斷是否已讀 2.修正了管理員dashboard下方的評論列表於無評論會顯示的提示 其colspan數值錯誤

---
+++ WebApp/Scripts/Admin/dismiss-news-alert.js
@@ -0,0 +1,7 @@
+﻿if (document.cookie.indexOf("dismissAlert=true") < 0) {
+    $("#alert-link").removeClass("hidden");
+}
+
+function rememberDismiss() {
+    document.cookie = "dismissAlert=true; path=/admin; sameSite=strict"
+}
